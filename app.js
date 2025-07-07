b065bb74ae495b2d3c7b544445f2fe6c7c6b3612
Rajaabpro <rajaab.pro@gmail.com>
2025-07-07 12:57:41 +0500

    Comment out existing logic in app.js for traffic light, age check, and popcorn size, while adding a new pass/fail condition based on marks.

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
-// traffic light
-let color = "red";
-if (color === "red" ){
-    console.log("stop");
-}
-else if (color === "yellow"){
-    console.log("slow down");
-}
-else if (color === "green"){
-    console.log("go");
-}
-console.log("end of the program");
+// // traffic light
+// let color = "red";
+// if (color === "red" ){
+//     console.log("stop");
+// }
+// else if (color === "yellow"){
+//     console.log("slow down");
+// }
+// else if (color === "green"){
+//     console.log("go");
+// }
+// console.log("end of the program");
 
-let age = 12;
-if (age >= 18){
-    console.log("you can vote");
-}
-else if (age <18){
-    console.log("you are a teenager");
-}
+// let age = 12;
+// if (age >= 18){
+//     console.log("you can vote");
+// }
+// else if (age <18){
+//     console.log("you are a teenager");
+// }
 
-// popcorn size
+// // popcorn size
 
-let popcornSize = "medium";
-if (popcornSize === "xlarge"){
-    console.log("The price is 250");
-}
-else if (popcornSize === "large"){ 
-    console.log("The price is 200");
-}
-else if (popcornSize === "medium"){
-    console.log("The price is 150");
-}
-else if (popcornSize === "small"){
-    console.log("The price is 100");
-}
-else{
-    console.log("Invalid size");
+// let popcornSize = "medium";
+// if (popcornSize === "xlarge"){
+//     console.log("The price is Rs. 250");
+// }
+// else if (popcornSize === "large"){ 
+//     console.log("The price is Rs. 200");
+// }
+// else if (popcornSize === "medium"){
+//     console.log("The price is Rs. 150");
+// }
+// else if (popcornSize === "small"){
+//     console.log("The price is Rs. 100");
+// }
+// else{
+//     console.log("Invalid size");
+// }
+
+// let marks = 100;
+// if (marks >= 90){
+//     console.log("You got A grade");
+//     if (marks >= 95){
+//         console.log("You got A+ grade");
+//     }
+// }
+// else if (marks >= 80){
+//     console.log("You got B grade");
+// }
+// else {
+//     console.log("You got C grade");
+// }
+
+let marks = 20;
+
+if ((marks > 33 && marks <= 80  ) || !false){
+    console.log("pass");
+    console.log("A+");
 }
