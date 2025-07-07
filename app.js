e43e78c2f4737f48e71b469a1d058dfa4e10899d
Rajaabpro <rajaab.pro@gmail.com>
2025-07-07 12:11:43 +0500

    Add traffic light, age check, and popcorn size logic to app.js for initial program functionality.

---
+++ app.js
@@ -0,0 +1,39 @@
+// traffic light
+let color = "red";
+if (color === "red" ){
+    console.log("stop");
+}
+else if (color === "yellow"){
+    console.log("slow down");
+}
+else if (color === "green"){
+    console.log("go");
+}
+console.log("end of the program");
+
+let age = 12;
+if (age >= 18){
+    console.log("you can vote");
+}
+else if (age <18){
+    console.log("you are a teenager");
+}
+
+// popcorn size
+
+let popcornSize = "medium";
+if (popcornSize === "xlarge"){
+    console.log("The price is 250");
+}
+else if (popcornSize === "large"){ 
+    console.log("The price is 200");
+}
+else if (popcornSize === "medium"){
+    console.log("The price is 150");
+}
+else if (popcornSize === "small"){
+    console.log("The price is 100");
+}
+else{
+    console.log("Invalid size");
+}
