31166b77e3ffd7ce26c8cf7d415bd876ced8a1c4
Rajaabpro <rajaab.pro@gmail.com>
2025-07-08 09:56:53 +0500

    Add day switch statement to app.js for day of the week logging functionality

--- app.js
+++ app.js
@@ -97,3 +97,32 @@ switch (color) {
         default:
             console.log("invalid color");
 }
+
+
+let day = "7";
+switch (day){
+    case "1":
+        console.log("Monday");
+        break;
+    case "2":
+        console.log("Tuesday");
+        break;
+    case "3":
+            console.log("Wednesday");
+            break;
+    case "4":
+            console.log("Thursday");
+            break;    case "5":
+            console.log("Friday");
+            break;
+     case "6":
+            console.log("Saturday");
+            break;    case "7":
+            console.log("Sunday");
+            break;
+    default:
+        console.log("Invalid day");
+        break;
+ 
+
+}
