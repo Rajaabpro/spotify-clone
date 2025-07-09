3a15071afa27378695af1c294ab973fbd350ed0c
Rajaabpro <rajaab.pro@gmail.com>
2025-07-09 11:28:36 +0500

    Comment out existing logic in app.js for various string and number validations, while retaining the new string manipulation functionality.

--- app.js
+++ app.js
@@ -56,73 +56,176 @@
 
 
 
-let str = "HET";
-if ((str === str.toUpperCase()) && str.length >2){
-    console.log(`${str} good string`);
-}
-else{
-    console.log(`${str}not a  good string`);
-}
+// let str = "HET";
+// if ((str === str.toUpperCase()) && str.length >2){
+//     console.log(`${str} good string`);
+// }
+// else{
+//     console.log(`${str}not a  good string`);
+// }
 
 
-let num = 7;
+// let num = 7;
 
-if ((num % 2 === 1) && ((num + 2 === 9) || (num - 1 === 5))) {
-    console.log("Valid");
-} else {
-    console.log("Invalid");
-}
-
-
-let name = "RAJA";
-
-if ((name === name.toUpperCase()) && (name.length > 3)) {
-    console.log("SHOUT");
-} else {
-    console.log("whisper");
-}
+// if ((num % 2 === 1) && ((num + 2 === 9) || (num - 1 === 5))) {
+//     console.log("Valid");
+// } else {
+//     console.log("Invalid");
+// }
 
 
-let color = "red";
-switch (color) {
-    case "red":
-        console.log("stop");
-        break;
-    case "yellow":
-        console.log("slow down");
-        break;
-    case "green":
-        console.log("go");
-        break
-        default:
-            console.log("invalid color");
-}
+// let userName = "RAJA";
+
+// if ((userName === userName.toUpperCase()) && (userName.length > 3)) {
+//     console.log("SHOUT");
+// } else {
+//     console.log("whisper");
+// }
 
 
-let day = "7";
-switch (day){
-    case "1":
-        console.log("Monday");
-        break;
-    case "2":
-        console.log("Tuesday");
-        break;
-    case "3":
-            console.log("Wednesday");
-            break;
-    case "4":
-            console.log("Thursday");
-            break;    case "5":
-            console.log("Friday");
-            break;
-     case "6":
-            console.log("Saturday");
-            break;    case "7":
-            console.log("Sunday");
-            break;
-    default:
-        console.log("Invalid day");
-        break;
- 
+// let color = "red";
+// switch (color) {
+//     case "red":
+//         console.log("stop");
+//         break;
+//     case "yellow":
+//         console.log("slow down");
+//         break;
+//     case "green":
+//         console.log("go");
+//         break
+//         default:
+//             console.log("invalid color");
+// }
 
-}
+
+// let number = 50;
+// if(number%10 === 0){
+//     console.log("good ");
+
+// }else{
+//     console.log("bad ");
+// }
+
+// let name = prompt("Enter your name");
+// let age = prompt("Enter your age");
+// alert(`${name} is ${age} years old.`);
+
+
+// let quarter = 4
+// switch (quarter) {
+//     case 1:
+//         console.log("January,February,March");
+//         break;
+//     case 2:
+//         console.log("April,May,June");
+//         break;
+//     case 3:
+//         console.log("July,August,September");
+//         break;
+//     case 4:
+//         console.log("October,November,December");
+//         break;
+//     default:
+//         console.log("Invalid quarter");
+// }
+
+// let str = "Amanullah";
+// if ((str[0] == "A" || str[0] == "a") && str.length > 5){
+//     console.log("gold string");
+// }
+// else{
+//     console.log("silver string");
+// }
+
+
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+
+// if (a > b && a > c){
+//     console.log(`${a} is greater`);
+// }
+// else if (b > a && b > c){
+//     console.log(`${b} is greater`);
+// }
+// else{
+//     console.log(`${c} is greater`);
+// }
+
+
+
+
+
+
+
+// let a = 10;
+// let b = 0;
+// let c = 3;
+
+// if (a > b){
+//     if (a>c){
+//         console.log(`${a} is greater`);
+//     }
+//     else{
+//         console.log(`${c} is greater`);
+//     }
+// }
+// else{
+//     if (b>c){
+//         console.log(`${b} is greater`);
+//     }
+//     else{
+//         console.log(`${c} is greater`);
+//     }
+// }
+
+
+// let yourNum = 5;
+// if (yourNum %2 == 0){
+//     console.log(`${yourNum} is even`);
+// }
+// else{
+//     console.log(`${yourNum} is odd`);
+// }
+
+
+// let num = 4;
+// if (num % 3 == 0){
+//     console.log("Fizz");
+
+// }else if (num % 5 == 0){
+//     console.log("Buzz");
+// }
+// else {
+//     console.log("Not divisible by 3 or 5");
+// }
+
+// let age = 19;
+// if (age < 13 ){
+//     console.log("You are a child");
+// }else if (age >= 13 && age <= 18){
+//     console.log("You are a teenager");
+// }else if (age >= 19 && age <= 60){
+//     console.log("You are an adult");
+// }else{
+//     console.log("You are a senior citizen");
+// }
+
+
+// if("A"){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+let str = "apnacollege";
+console.log(str.slice(4).replace("l","t"));
+let str2 = str.slice(4).replace("l","t");
+console.log(str2.replace("l","t"));
+
+
+
