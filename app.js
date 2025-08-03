cc043a90191d2afeb007812a74ca89a00edfa182
Rajaabpro <rajaab.pro@gmail.com>
2025-08-03 12:04:47 +0500

    Comment out the getFact function and related code in app.js to disable cat fact fetching, improving code clarity and maintainability.

--- app.js
+++ app.js
@@ -667,25 +667,25 @@
 
 
 
-let url = "https://catfact.ninja/fact";
+// let url = "https://catfact.ninja/fact";
 
-async function getFact(){
-    try{    
-        let res = await fetch(url);
-        let data = await res.json();
-        console.log(data.fact);
+// async function getFact(){
+//     try{    
+//         let res = await fetch(url);
+//         let data = await res.json();
+//         console.log(data.fact);
 
-        let res2 = await fetch(url);
-        let data2 = await res2.json();
-        console.log(data2.fact);
+//         let res2 = await fetch(url);
+//         let data2 = await res2.json();
+//         console.log(data2.fact);
         
-    }
-    catch(e){
-        console.log(e);
-    }
-    console.log("hello");
-}
-console.log("hello323");
+//     }
+//     catch(e){
+//         console.log(e);
+//     }
+//     console.log("hello");
+// }
+// console.log("hello323");
     
-    getFact();
+//     getFact();
  
