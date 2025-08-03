117ae764a280605a438a6c6a928a19ee3c91f0e2
Rajaabpro <rajaab.pro@gmail.com>
2025-08-03 12:02:32 +0500

    Enhance getFact function in app.js to fetch and log multiple cat facts, adding error handling and improving data retrieval process.

--- app.js
+++ app.js
@@ -670,6 +670,22 @@
 let url = "https://catfact.ninja/fact";
 
 async function getFact(){
-    fetch(url)
-}
+    try{    
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
 
+        let res2 = await fetch(url);
+        let data2 = await res2.json();
+        console.log(data2.fact);
+        
+    }
+    catch(e){
+        console.log(e);
+    }
+    console.log("hello");
+}
+console.log("hello323");
+    
+    getFact();
+ 
